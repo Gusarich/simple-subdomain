2835ca548d03488f49fd86fe96d4e2ab016e7df2
Gusarich <danielsedovzzz@gmail.com>
2023-06-03 21:00:30 +0300

    little fix

--- scripts/setSubdomainWallet.ts
+++ scripts/setSubdomainWallet.ts
@@ -4,13 +4,20 @@ import { NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
     const subdomainManager = provider.open(
-        SubdomainManager.createFromAddress(Address.parse('EQApEmWDFvpO0HkLv-QxHucmhsZ2QNEOiBX5MmUsien0Uaqj'))
+        SubdomainManager.createFromAddress(Address.parse('EQCuUtg1jrIuLDWHRoNWxALr5iAib5or4yUEOyyi5C43MHtf'))
     );
 
     await subdomainManager.sendSetWallet(
         provider.sender(),
         toNano('0.02'),
-        'test',
+        '1',
         Address.parse('EQBKgXCNLPexWhs2L79kiARR1phGH1LwXxRbNsCFF9doc2lN')
     );
+
+    // await subdomainManager.sendSetNextResolver(
+    //     provider.sender(),
+    //     toNano('0.02'),
+    //     '2',
+    //     Address.parse('EQCuUtg1jrIuLDWHRoNWxALr5iAib5or4yUEOyyi5C43MHtf')
+    // );
 }
